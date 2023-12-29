469c05f7de1deecfc4b1334c676cc864e5969e44
Oğuzhan Özenç <110353261+oguzhanozenc@users.noreply.github.com>
2023-12-29 21:24:44 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+// SELECTORS
+const hour = document.getElementById("hour");
+const min = document.getElementById("minutes");
+const sec = document.getElementById("seconds");
+const ampm = document.getElementById("ampm");
+
+// FUNCTIONS
+function updateClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampmvalue = "AM";
+
+  if (h > 12) {
+    h = h - 12;
+    ampmvalue = "PM";
+  }
+
+  hour.innerText = h;
+  min.innerText = m;
+  sec.innerText = s;
+  ampm.innerText = ampmvalue;
+
+  setTimeout(() => {
+    updateClock();
+  }, 1000);
+}
+
+updateClock();
