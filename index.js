df8202af4bc2a2b6bd63408496a89f29accb3bf7
Oğuzhan Özenç <110353261+oguzhanozenc@users.noreply.github.com>
2023-12-29 21:56:26 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function updateClock() {
   }
 
   h = h < 10 ? "0" + h : h;
-  m = m < 10 ? "0" + h : h;
-  s = s < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
 
   hour.innerText = h;
   min.innerText = m;
